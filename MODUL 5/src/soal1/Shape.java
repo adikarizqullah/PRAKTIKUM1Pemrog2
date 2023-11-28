77167c044b9d880dd0f8a4d0b7822b881fbbb0d6
adikarizqullah <115325015+adikarizqullah@users.noreply.github.com>
2023-11-28 12:47:43 +0800

    Delete MODUL 5 directory

--- MODUL 5/src/soal1/Shape.java
+++
@@ -1,8 +0,0 @@
-package soal1;
-
- public abstract class Shape {
-        protected String shapeName;
-        public Shape (String name){shapeName = name;}
-        protected abstract Double area();
-        public String toString(){return shapeName;}
-    }
