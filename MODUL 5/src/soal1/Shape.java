e5dfd372768a40f29a02ebe2c578b5fea30ce868
adikarizqullah <115325015+adikarizqullah@users.noreply.github.com>
2023-11-28 12:48:47 +0800

    Add files via upload

---
+++ MODUL 5/src/soal1/Shape.java
@@ -0,0 +1,8 @@
+package soal1;
+
+ public abstract class Shape {
+        protected String shapeName;
+        public Shape (String name){shapeName = name;}
+        protected abstract Double area();
+        public String toString(){return shapeName;}
+    }
